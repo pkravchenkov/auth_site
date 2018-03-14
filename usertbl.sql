05fc23fc7b8211c130419c06353826cb4a74ca74
pkravchenkov <p.kravchenkov@gmail.com>
2018-03-14 16:04:13 +0300

    add db

---
+++ usertbl.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 14 2018 г., 14:00
+-- Версия сервера: 10.1.30-MariaDB
+-- Версия PHP: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `userlistdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `usertbl`
+--
+
+CREATE TABLE `usertbl` (
+  `id` int(12) NOT NULL,
+  `full_name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(256) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `usertbl`
+--
+
+INSERT INTO `usertbl` (`id`, `full_name`, `email`, `username`, `password`) VALUES
+(1, 'Diman Popov', 'diman@tande.ru', 'diman', '123'),
+(3, 'Pavel', 'iii@gmaio.ru', 'Pavel', '1337');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `usertbl`
+--
+ALTER TABLE `usertbl`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `usertbl`
+--
+ALTER TABLE `usertbl`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
